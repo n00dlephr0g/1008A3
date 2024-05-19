43893ca1735829332fa1dc0cc97f3d9a921579e7
sch00lphr0g <161262859+sch00lphr0g@users.noreply.github.com>
2024-05-20 00:53:41 +1000

    completed docstrings

--- mode1.py
+++ mode1.py
@@ -12,9 +12,11 @@ class Mode1Navigator:
             this list is sorted at the start based on the gold to guardian ratio.
             the choice to keep the sites attribute as a sorted python list instead of using other
             data structures is due to its ease of use and the simplicity of list data structures.
+
         adventurers
             an integer of how many adventurers the Mode1Navigator has to its disposal.
             can change over time.
+
         length:
             an integer of how many lands are in the list.
             is instanciated for convenience in 2 of the methods.
@@ -25,21 +27,27 @@ class Mode1Navigator:
             also sorts the sites list using mergesort provided before storing it.
             the mergesort makes the constructor have a TC of O(n*log(n))
             where n is the size of the provided sites list
+
         select_sites()
             returns the optimal list of sites to invade and amount of adventurers to send to each.
             total amount of adventurers to send does not exceed the amount of adventurers on hand.
             since the sites variable was already sorted in the init method, this function goes 
-            through each Land object 1-by-1 (in optimal order) until there will be no troops left
-            to send.
+            through each Land objects in optimal order until there will be no troops left to send.
             doesnt actually modify any objects in self.sites or change the value of self.adventurers.
             the loop included makes this method have a TC of O(n)
             where n is the length of the sites list
+
         select_sites_from_adventure_numbers()
             returns a list of maximum rewards that can be reached given a list of adventurer counts.
-            uses similar logic as select_sites() but calls that loop for every different adventurer
-            count. this means the logic includes 2 loops
+            uses a similar loop as select_sites() but calls that loop for every different adventurer
+            count. this means the logic includes 2 loops, one for inputs and one for sites.
+            these loops brings the complexity to O(n*m)
+            where n is the length of sites
+            where m is the length of inputs
 
-        
+        update_sites()
+            updates a Land object
+            nothing special, just uses Land.set_guardians() and Land.set_gold() to update the object.
 
     Student-TODO: short paragraph as per
     https://edstem.org/au/courses/14293/lessons/46720/slides/318306
@@ -47,6 +55,7 @@ class Mode1Navigator:
     
     
     """
+    
     def __init__(self, sites: list[Land], adventurers: int) -> None:
         """
         __init__ constructor for mode 1 navigator class
