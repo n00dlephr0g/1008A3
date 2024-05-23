771b525bd2813e24791c3d2d2b22647fe722b8fa
sch00lphr0g <161262859+sch00lphr0g@users.noreply.github.com>
2024-05-23 23:10:40 +1000

    update

--- mode2.py
+++ mode2.py
@@ -6,13 +6,16 @@ class Mode2Navigator:
     Student-TODO: short paragraph as per
     https://edstem.org/au/courses/14293/lessons/46720/slides/318306
         :complexity best: 
-        :complexity best: 
         :complexity worst:
     """
 
     def __init__(self, n_teams: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        __init__ initialises a game given a number of teams
+
+        :param n_teams: an integer amount of teams
+        :complexity best: O(1)
+        :complexity worst: O(1)
         """
         self.teamCount = n_teams
         self.sites: list[Land] = []
@@ -20,7 +23,12 @@ class Mode2Navigator:
 
     def add_sites(self, sites: list[Land]) -> None:
         """
-        Student-TODO: Best/Worst Case
+        add_sites stores the given sites into self
+
+        :param sites: list of sites to add
+        :complexity best: O(n) 
+        :complexity worst: O(n)
+        :variable n: the length of sites
         """
         for site in sites:
             if site.get_ratio() > 2.5:
@@ -29,7 +37,13 @@ class Mode2Navigator:
 
     def simulate_day(self, adventurer_size: int) -> list[tuple[Land | None, int]]:
         """
-        Student-TODO: Best/Worst Case
+        simulate_day runs the game logic given the adventurers that the team will have and returns a list of decisions that each team has made (the land chosen and teh amount that was sent to the land)
+
+
+        :param adventurer_size: the amount of adventurers available to each team
+        :return: a list of decisions that each team has made
+        :complexity best: 
+        :complexity worst:
         """
         self.sites = scoreMergeSort(self.sites, adventurer_size)
         out = []
